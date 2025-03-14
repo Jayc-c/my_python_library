ff0e3b23614c8c2079cdd3398ef33f74af1e9cf1
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 22:11:58 +0800

    prompt user input

--- sorting_number_lowest_to_highest.py.py
+++ sorting_number_lowest_to_highest.py.py
@@ -1,5 +1,11 @@
 # Initialize an empty list to store numbers.
+numbers = []
+
 # Prompt the user to input a number.
+while True:
+    try:
+        value = float(input("Enter a number: "))
+
 # Store the number.
 # Sort the list of number.
 # Display current sorted list.
