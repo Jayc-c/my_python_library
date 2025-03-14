071d8860be8da33e0ada55a800b28712e4651fbf
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 21:14:44 +0800

    prompt user input

--- unique_duplicate_check.py
+++ unique_duplicate_check.py
@@ -1,7 +1,15 @@
 # Track Entered Numbers (using a list instead of a set)
+seen_numbers = []
+
 # Prompt the user to input a number. (Loop)
-# Validate number input.
-# heck for duplicates
+while True:
+    try:
+        user_input = input("Enter a number (or anything else to stop): ")
+
+        # Validate number input.
+        number = float(user_input)
+    
+# Check for duplicates.
 # Display Unique/Duplicate Status.
 # Display Unique/Duplicate Status.
 # Record Unique Number.
