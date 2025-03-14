ca577c091f160e49fd4baf1716449c37c94c056e
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 09:42:50 +0800

    made the code simpler then compared and print the bigger number

--- print_bigger_number_of_2_numbers.py
+++ print_bigger_number_of_2_numbers.py
@@ -1,7 +1,12 @@
-# Prog01: Create a program that ask user to input 2 numbers. 
+# Ask user to input 2 numbers. 
 
-def bigger_number():
-    num1 = int(input("Input a number: "))
-    num2 = int(input("Input a number: "))
+
+num1 = int(input("Input a number: "))
+num2 = int(input("Input a number: "))
 
 # Print the bigger number.
+
+if num1 > num2:
+    print ("The bigger number is:", num1)
+else:
+    print ("The bigger number is:", num2)
