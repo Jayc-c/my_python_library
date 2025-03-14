f86d26b60daac53e6c6a081f864e416f54144cb4
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 21:09:55 +0800

    pseudocode

---
+++ unique_duplicate_check.py
@@ -0,0 +1,8 @@
+# Track Entered Numbers (using a list instead of a set)
+# Prompt the user to input a number. (Loop)
+# Validate number input.
+# heck for duplicates
+# Display Unique/Duplicate Status.
+# Display Unique/Duplicate Status.
+# Record Unique Number.
+# Stop on invalid input.
