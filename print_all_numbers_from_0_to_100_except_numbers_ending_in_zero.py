94364fd8525296366a72acc8914d723f309b505c
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 17:04:36 +0800

    created loop and used remainder to check

--- print_all_numbers_from_0_to_100_except_numbers_ending_in_zero.py
+++ print_all_numbers_from_0_to_100_except_numbers_ending_in_zero.py
@@ -1,3 +1,5 @@
 # Create a loop iterating numbers from 0 to 100
+for i in range(101):
 # Check if the number ends in 0, by dividing to 10 and won't equal to 0.
+    if i % 10 != 0 or i == 0:
 # Print if the remainder is not equal to 0. It means the number doesn't end in 0.
