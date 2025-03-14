37eedb666606c4f8ca7afd0487210eff5b049bb3
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 11:15:04 +0800

    pseudocode and user input

---
+++ print_equal_numbers.py
@@ -0,0 +1,7 @@
+# Ask user to input 2 numbers. 
+
+num1 = int(input("Input a number: "))
+num2 = int(input("Input a number: "))
+
+# Compare if the numbers are equal
+# Print "Equal" when the numbers are the same.
