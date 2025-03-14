d9c1533ad6944b537e529a044ef08c32020998b9
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 21:58:06 +0800

    process added

--- number_tracker_lowest_number.py
+++ number_tracker_lowest_number.py
@@ -1,4 +1,4 @@
-# Initialize an empty list to store numbers
+# Initialize an empty list to store numbers.
 entered_numbers = []
 
 # Prompt the user to input a number.
@@ -6,7 +6,11 @@ while True:
     try:
         num = int(input("Enter a number: "))  
 
-# Stores the number.
-# check if the list is not empty before finding min.
-# Show the lowest numbe.
+        # Stores the number
+        entered_numbers.append(num)  
+        # check if the list is not empty before finding min.
+        if entered_numbers: 
+            # Show the lowest number.
+            print(f"Lowest number so far: {min(entered_numbers)}")
+
 # stops the program if invalid input.
