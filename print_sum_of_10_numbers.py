7bff38744c06808e2c216b2ace5f1148d8ebfe59
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 15:55:55 +0800

    added the loop and variable

--- print_sum_of_10_numbers.py
+++ print_sum_of_10_numbers.py
@@ -1,5 +1,9 @@
 # Initialize the sum variable to 0, as a starting point.
+sum = 0
 # Loop 10 times, using 'i' in range to 10.
-# Ask the user to input a number per loop
-# Add the input number to the sum variable.
+for i in range(10):
+    # Ask the user to input a number per loop
+    num = float(input(f"enter a number {i+1}"))
+    # Add the input number to the sum variable.
+    sum += num
 # Print the sum of all the numbers.
