b2fb317438ae215fae0da26e7e463e16eeaaa15f
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 15:09:36 +0800

    user input

--- power_number.py
+++ power_number.py
@@ -1,3 +1,7 @@
 # Ask user to input 2 numbers. 
+
+base_num1 = int(input("Input a number: "))
+exponent_num2 = int(input("Input another number: "))
+
 # Raise the first number to the second one using "**".
 # Print result.
