d0bcafa5370ca2e29da948e530e70daa7d299924
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-15 00:31:00 +0800

    pseudocode

---
+++ calculate_average.py
@@ -0,0 +1,9 @@
+# Initialize and empty list to store numbers.
+# Loop until invalid input.
+# Prompt user to input a number  
+# Store number to the list.
+# Stop on non-number input
+# If at least one number was entered.
+# Calculate average.
+# Print the average.
+# Else, no input message.
