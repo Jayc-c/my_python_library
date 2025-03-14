2b5f4dffee0ebec8598128783be9dd4f2d2d81c1
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 23:50:22 +0800

    process added

--- frequent_duplicate_number.py
+++ frequent_duplicate_number.py
@@ -1,8 +1,19 @@
-# Initialize an empty list to store numbers.
-# Prompt user to input numbers. 
-# Store the number to the list.
-# Get the unique numbers from the list.
-# Find the number that appears most often.
-# Count how many times the most frequent number appears.
+ # Initialize an empty list to store numbers.
+entered_numbers = [] 
+
+# Prompt user to input numbers.
+while True:
+    try:
+        num = int(input('Enter number: '))   
+        # Store the number to the list.
+        entered_numbers.append(num)
+
+        # Get the unique numbers from the list.       
+        unique_numbers = set(entered_numbers)
+        # Find the number that appears most often.
+        most_frequent = max(unique_numbers, key=entered_numbers.count)
+        # Count how many times the most frequent number appears.
+        count = entered_numbers.count(most_frequent)
+
 # Display the most frequent number and its count.
 # Stops the program if invalid input.     
