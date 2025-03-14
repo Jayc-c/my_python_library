f3048805fb26aae65e08de4083b7cc93587451ad
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 23:46:35 +0800

    pseudocode

---
+++ frequent_duplicate_number.py
@@ -0,0 +1,8 @@
+# Initialize an empty list to store numbers.
+# Prompt user to input numbers. 
+# Store the number to the list.
+# Get the unique numbers from the list.
+# Find the number that appears most often.
+# Count how many times the most frequent number appears.
+# Display the most frequent number and its count.
+# Stops the program if invalid input.     
