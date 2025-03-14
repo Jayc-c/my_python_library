ca95c1c6effbaadd141e2f882362fa2fc67d33f0
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 18:27:59 +0800

    prompts the user to input 10 numbers

--- print_difference_of_10 number.py
+++ print_difference_of_10 number.py
@@ -1,6 +1,10 @@
 # Initialize an empty list
-# Loop 10 times, using 'i' in range to 10.
-# Ask the user to input a number per loop
-# Add the input number to the list.
+numbers = []
+    # Loop 10 times, using 'i' in range to 10.
+for i in range(10):
+    # Ask the user to input a number per loop
+    num = int(input(f"Enter number {i+1}: "))
+    # Add the input number to the list.
+    numbers.append(num)
 # process
 # Print the result of the first number minus all the remaining numbers.
