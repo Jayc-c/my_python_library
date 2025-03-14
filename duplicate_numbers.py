8c1ed13dd97c5f16a01f0dbda21dda08a0222035
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 23:26:05 +0800

    pseudocode

---
+++ duplicate_numbers.py
@@ -0,0 +1,5 @@
+# Initialize an empty list for storing entered number
+# Ask user 10 numbers
+# Initialize an empty list for counting duplicate numbers
+# Track duplicates
+# display numbers that have duplicate
