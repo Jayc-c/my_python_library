bc3250e16d4b8326c9565b8228ed215255e630d6
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 17:01:14 +0800

    pseudocode

---
+++ print_all_numbers_from_0_to_100_except_numbers_ending_in_zero.py
@@ -0,0 +1,3 @@
+# Create a loop iterating numbers from 0 to 100
+# Check if the number ends in 0, by dividing to 10 and won't equal to 0.
+# Print if the remainder is not equal to 0. It means the number doesn't end in 0.
