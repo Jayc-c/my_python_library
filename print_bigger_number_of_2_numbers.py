c23e5fddd537b84197b0c5a24bf5789eb5882dd1
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 09:32:52 +0800

    pseudocode and asked user to input 2 numbers

---
+++ print_bigger_number_of_2_numbers.py
@@ -0,0 +1,7 @@
+# Prog01: Create a program that ask user to input 2 numbers. 
+
+def bigger_number():
+    num1 = int(input("Input a number: "))
+    num2 = int(input("Input a number: "))
+
+# Print the bigger number.
