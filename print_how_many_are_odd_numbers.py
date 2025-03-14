01233a38ec061615b5694a1c2e5a2f2d274aa245
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 16:29:04 +0800

    pseudocode

---
+++ print_how_many_are_odd_numbers.py
@@ -0,0 +1,4 @@
+# Initialize odd_count variable to 0.
+# Loop 10 times, prompt the user to input a number per loop.
+# Check if the input number is divisible by 2, if yes, add 1 to the variable.
+# Print the amount of even numbers.
