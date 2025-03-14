25f0463b4e6a5aea3a90e23859323b705a878d53
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 15:11:59 +0800

    raise the 1st number to the 2nd one using **.

--- power_number.py
+++ power_number.py
@@ -4,4 +4,7 @@ base_num1 = int(input("Input a number: "))
 exponent_num2 = int(input("Input another number: "))
 
 # Raise the first number to the second one using "**".
+
+result = base_num1**exponent_num2
+
 # Print result.
