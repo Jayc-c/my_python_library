3758cd6351e12b3a5569bb235d652d5a69817098
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 16:50:41 +0800

    loop and checks if number's even

--- print_all_even_numbers_from_0_to_100.py
+++ print_all_even_numbers_from_0_to_100.py
@@ -1,3 +1,5 @@
 # Create a loop that iterates through numbers from 0 to 100.
-# Check if the current number is even.
+for i in range(101):
+    # Check if the current number is even.
+    if i % 2 == 0:
 # If it's even, print the number.
