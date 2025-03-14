78a01a29f7870afbf8db766dd26aa17de42f18ee
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 19:14:17 +0800

    find range start and end

--- print_all_numbers_between_2_numbers.py
+++ print_all_numbers_between_2_numbers.py
@@ -3,4 +3,7 @@ num1 = int(input("Input a number: "))
 num2 = int(input("Input a number: "))
 
 # Find range start and end.
+start = min(num1, num2) + 1
+end = max(num1, num2)
+
 # Print numbers in range.
