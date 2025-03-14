115eba295922255b0199a5bb5056b11e882ba054
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 15:49:22 +0800

    psuedocode

---
+++ print_sum_of_10_numbers.py
@@ -0,0 +1,5 @@
+# Initialize the sum variable to 0, as a starting point.
+# Loop 10 times, using 'i' in range to 10.
+# Ask the user to input a number per loop
+# Add the input number to the sum variable.
+# Print the sum of all the numbers.
