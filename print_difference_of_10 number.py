336f1691fb0f358ea160463121db8c3ffae4dc2e
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 18:24:10 +0800

    pseudocode

---
+++ print_difference_of_10 number.py
@@ -0,0 +1,6 @@
+# Initialize an empty list
+# Loop 10 times, using 'i' in range to 10.
+# Ask the user to input a number per loop
+# Add the input number to the list.
+# process
+# Print the result of the first number minus all the remaining numbers.
