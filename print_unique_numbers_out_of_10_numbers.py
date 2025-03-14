20f16fe22a8e4a844e69d3316ec16a018c7c9cea
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 19:32:42 +0800

    pseudocode

---
+++ print_unique_numbers_out_of_10_numbers.py
@@ -0,0 +1,9 @@
+# Initialize an empty list to store the numbers entered by the user.
+# Loop 10 times using 'i' in range to 10
+# Prompt the user input a number.
+# Add the entered number to the 'numbers' list.
+# Initialize an empty list to store the numbers that appear only once.
+# Loop through each number in the 'numbers' list.
+# Check if the current number appears only once.
+# If yes, add to the 'unique_numbers' list.
+# Print the list of numbers that don't have duplicates.
