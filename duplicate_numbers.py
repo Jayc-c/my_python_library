c0b7b67b88181b6c32a791cd63dab6f7d936765c
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 23:01:58 +0800

    pseudocode

---
+++ duplicate_numbers.py
@@ -0,0 +1,7 @@
+# Initialize an empty list to store the numbers entered by the user.
+# Prompt the user to input 20 numbers.
+# Store the number. 
+# Display the numbers that don't have duplicates
+# Check each number in the list
+# Check if unique.
+# Print unique number.
