f91c8521e55780adb8d523a28df356f7a2fc6dbc
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 23:26:44 +0800

    prompt user input

--- duplicate_numbers.py
+++ duplicate_numbers.py
@@ -1,5 +1,11 @@
 # Initialize an empty list for storing entered number
+entered_numbers = []
+
 # Ask user 10 numbers
+for i in range(10):
+    num = int(input(f"Enter number {i + 1}: "))
+    entered_numbers.append(num)
+
 # Initialize an empty list for counting duplicate numbers
 # Track duplicates
 # display numbers that have duplicate
