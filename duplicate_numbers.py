d1da722f68436268a63a14a64121807c40fa392e
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 23:30:18 +0800

    display numbers that have duplicate

--- duplicate_numbers.py
+++ duplicate_numbers.py
@@ -1,17 +1,21 @@
-# Initialize an empty list for storing entered number
+# Initialize an empty list for storing entered number.
 entered_numbers = []
 
-# Ask user 10 numbers
+# Ask user 10 numbers.
 for i in range(10):
     num = int(input(f"Enter number {i + 1}: "))
     entered_numbers.append(num)
 
-# Initialize an empty list for counting duplicate numbers
+# Initialize an empty list for counting duplicate numbers.
 duplicate_numbers = []
 
-# Track duplicates
+# Track duplicates.
 for num in entered_numbers:
     if entered_numbers.count(num) > 1 and num not in duplicate_numbers:
         duplicate_numbers.append(num)
 
-# Display numbers that have duplicate
+# Display numbers that have duplicate.
+if duplicate_numbers:
+    print(f"The numbers that have duplicates are {duplicate_numbers}")
+else:
+    print("No duplicate numbers were found.")
