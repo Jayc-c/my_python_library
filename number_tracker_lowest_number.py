29cfb0912251af5f0c0916f11e0d7ee09786c6c6
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 21:55:09 +0800

    pseudocode

---
+++ number_tracker_lowest_number.py
@@ -0,0 +1,6 @@
+# Initialize an empty list to store numbers.
+# Prompt the user to input a number.
+# Stores the number.
+# check if the list is not empty before finding min.
+# Show the lowest numbe.
+# stops the program if invalid input.
