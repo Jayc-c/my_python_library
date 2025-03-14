eaf5f31f77d712421ddb264630fa0e267f91e83c
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 23:03:16 +0800

    promt user inpnut

--- duplicate_numbers.py
+++ duplicate_numbers.py
@@ -1,6 +1,12 @@
 # Initialize an empty list to store the numbers entered by the user.
+numbers = [] 
+
 # Prompt the user to input 20 numbers.
-# Store the number. 
+for i in range(10):
+    num = int(input(f"Enter your {i + 1} number: "))  
+    # Store the number. 
+    numbers.append(num)  
+
 # Display the numbers that don't have duplicates
 # Check each number in the list
 # Check if unique.
