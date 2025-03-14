7ebbc41bafab6e98854beac03d3214c112579ba7
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-15 00:17:57 +0800

    pseudocode

---
+++ sort_numbers_descending.py
@@ -0,0 +1,8 @@
+# List to store numbers.
+# Loop until invalid input.
+# Prompt user to input a number
+# Store the number to list.
+# Stop on non-number input.
+# Sort from highest to lowest.
+# Display the sorted numbers.
+# Else, no input message.
