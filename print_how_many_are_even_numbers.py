adfcabc3395168622798338d28b174a0430997d9
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 18:45:35 +0800

    check if the input number is even

--- print_how_many_are_even_numbers.py
+++ print_how_many_are_even_numbers.py
@@ -5,5 +5,8 @@ even_count = 0
 for i in range(10):
     # Prompt the user to input a number per loop.
     number = int(input(f"Enter number {i + 1}: "))
-# Check if the input number is divisible by 2, if yes, add 1 to the variable.
+
+    # Check if the input number is divisible by 2, if yes, add 1 to the variable.
+    if number % 2 == 0:
+        even_count += 1
 # Print the amount of even numbers.
