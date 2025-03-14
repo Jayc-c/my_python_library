5b6ffc4c61d3226d0ecc7e2afb52834e588a78dd
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-15 00:02:01 +0800

    initialized an empty list, prompt the user input, then stored to the list

--- highest_number_tracker.py
+++ highest_number_tracker.py
@@ -1,6 +1,13 @@
 # Initialize an empty list to store numbers
+numbers = []  
+
 # Prompt the user to input numbers
-# Store the number to the list.
+while True:
+    try:
+        num = float(input("Enter a number: "))
+        # Store the number to the list.
+        numbers.append(num)  
+
 # Check if the list is not empty.
 # Check if the list is not empty.
 # Find and print the max number
