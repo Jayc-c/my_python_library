165da29accb34dc1dbc7a237080f665d838a798a
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-15 00:00:37 +0800

    pseudocode

---
+++ highest_number_tracker.py
@@ -0,0 +1,7 @@
+# Initialize an empty list to store numbers
+# Prompt the user to input numbers
+# Store the number to the list.
+# Check if the list is not empty.
+# Check if the list is not empty.
+# Find and print the max number
+# Stop on invalid input.
