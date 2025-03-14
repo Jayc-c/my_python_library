2fa0e5ef8ffe72ed35149a5be1082162a476ab24
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-03-14 22:10:58 +0800

    pseudocode

---
+++ sorting_number_lowest_to_highest.py.py
@@ -0,0 +1,6 @@
+# Initialize an empty list to store numbers.
+# Prompt the user to input a number.
+# Store the number.
+# Sort the list of number.
+# Display current sorted list.
+# Stop on invalid input.
